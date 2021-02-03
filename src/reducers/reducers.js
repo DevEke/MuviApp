d8efbc719262835be1bd51d48e8e6f50dac84b53
Devke <chriseke13@gm>
2021-02-03 13:24:10 -0600

    Initialized reducer.js file

---
+++ src/reducers/reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import {
+    GET_MOVIES,
+    SET_USER,
+    REGISTER_USER,
+    ADD_TO_FAVORTIES,
+    REMOVE_FROM_FAVORITES,
+    UPDATE_USER,
+    UNREGISTER_USER
+} from '../actions/actions.js';
+
