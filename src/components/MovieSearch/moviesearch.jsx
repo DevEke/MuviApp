03dca741c09cc1419527590aebffe73de2a685b6
Devke <chriseke13@gm>
2021-02-04 14:51:25 -0600

    Populated MovieSearch component

--- src/components/MovieSearch/moviesearch.jsx
+++ src/components/MovieSearch/moviesearch.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { setFilter } from '../../actions/actions';
+
+function MovieSearch(props) {
+    return <input 
+        onChange={(e) => props.setFilter(e.target.value)}
+        value={props.movieFilter}
+        placeholder="Search"
+        />
+}
+
+export default connect(null, { setFilter })(MovieSearch);
