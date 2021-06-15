d8e1816cf917dd8da8bfab51951e27ef6b53a82e
Devke <chriseke13@gm>
2021-06-14 20:59:32 -0500

    updated Browsers List

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -41186,7 +41186,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58180" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55172" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
